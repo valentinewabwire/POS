45ed9306e834395e533c78eb0b354c3d0221dfe6
tracomclass <valentine.w.maita@gmail.com>
2020-02-01 11:03:22 +0300

    Initial commit

---
+++ BASE_APP/BSE_APP_v3/Obj/Resources.d
@@ -0,0 +1,9 @@
+E:\valentine\proffesion\Move2500\project\BASE_APP\BSE_APP_v3\GOAL\Resources\APCLESSGuiPresent.TBR;1523427602000
+E:\valentine\proffesion\Move2500\project\BASE_APP\BSE_APP_v3\GOAL\Resources\APCLESSGuiPresent.gml;1522225808000
+E:\valentine\proffesion\Move2500\project\BASE_APP\BSE_APP_v3\GOAL\Resources\APCLESSGuiText.TBR;1522157734000
+E:\valentine\proffesion\Move2500\project\BASE_APP\BSE_APP_v3\GOAL\Resources\APCLESSGuiText.gml;1522225808000
+E:\valentine\proffesion\Move2500\project\BASE_APP\BSE_APP_v3\GOAL\Resources\COL_NMB.JPG;1520411952000
+E:\valentine\proffesion\Move2500\project\BASE_APP\BSE_APP_v3\GOAL\Resources\Icons\CLess.WGU;1500990738000
+E:\valentine\proffesion\Move2500\project\BASE_APP\BSE_APP_v3\GOAL\Resources\Icons\COL_NMB.JPG;1520411952000
+E:\valentine\proffesion\Move2500\project\BASE_APP\BSE_APP_v3\GOAL\Resources\LOGO.png;1500990738000
+gmlToTbrConverter
